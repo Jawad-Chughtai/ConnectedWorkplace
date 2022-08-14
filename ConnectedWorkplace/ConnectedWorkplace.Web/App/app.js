4058f486af6224bd04512e645d745a666a2c9e44
Jawad Chughtai <jwaad04@gmail.com>
2022-08-15 01:55:17 +0500

    Installed AngularJs, EntityFramework,, Created app.js
    Created two projects, for web and code first approach. Installed AngularJs and EntityFramework and set up angular application

---
+++ ConnectedWorkplace/ConnectedWorkplace.Web/App/app.js
@@ -0,0 +1,27 @@
+﻿
+(function () {
+    'use strict'
+    angular
+        .module('myApp', ['ngRoute'])
+        .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+
+            $locationProvider.hashPrefix('');
+
+        $routeProvider
+            .when('/', {
+                templateUrl: 'Main/Views/Home/Home.html',
+                controller: 'HomeController'
+            })
+            .when('/home', {
+                redirectTo: '/'
+            })
+            .when('/people', {
+                templateUrl: 'Main/Views/People/People.html',
+                controller: 'PeopleController'
+            })
+            .otherwise({
+                redirectTo: '/'
+            })
+    }]);
+
+})();
